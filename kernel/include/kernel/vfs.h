4a461ba11d4ec1a4273e0fa8bde1cde1abbdd1e3
ryuukumar <adityakumar4644@gmail.com>
2026-03-17 20:38:40 +0530

    Add basic vfs definitions

---
+++ kernel/include/kernel/vfs.h
@@ -0,0 +1,36 @@
+#ifndef VFS_H
+#define VFS_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef enum { FILE, DIRECTORY } file_type_t;
+
+typedef struct inode inode;
+typedef struct file file;
+
+typedef struct {
+	int (*lookup) (struct inode* dir, struct inode* res, const char* name);
+} inode_operations;
+
+typedef struct {
+	int (*open) (inode*, file*);
+	int (*close) (inode*, file*);
+	int (*read) (inode*, file*, void*, size_t);
+	int (*seek) (inode*, file*, int);
+} file_operations;
+
+struct inode {
+	uint64_t i_no, i_sz;
+	void* i_pvt;
+	inode_operations* i_iops;
+	file_type_t i_type;
+};
+
+struct file {
+	inode* f_inode;
+	uint64_t f_pos, f_cnt;
+	file_operations* f_fops;
+};
+
+#endif
