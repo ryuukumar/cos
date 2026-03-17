8f6aaf9065091e796d4e1b41f5d57428b28cfd06
ryuukumar <adityakumar4644@gmail.com>
2026-03-17 21:01:56 +0530

    Invert strcmp logic

--- kernel/src/string.c
+++ kernel/src/string.c
@@ -92,6 +92,6 @@ void ulitos (uint64_t i, char* buf, uint32_t b) {
 int strcmp (const char* a, const char* b) {
 	for (int i = 0; a[i] != 0 && b[i] != 0; i++)
 		if (a[i] != b[i])
-			return 0;
-	return 1;
+			return 1;
+	return 0;
 }
