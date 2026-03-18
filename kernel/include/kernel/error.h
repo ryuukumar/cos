68a24ee7e515a2aaf8dc4279894188546cff7b1e
ryuukumar <adityakumar4644@gmail.com>
2026-03-18 20:59:03 +0530

    Move errors to error.h

---
+++ kernel/include/kernel/error.h
@@ -0,0 +1,15 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+// No root inode
+#define ENOROOT 500
+// Invalid argument
+#define EINVARG 501
+// Need an absolute path
+#define ENEEDABS 502
+// Invalid path
+#define EINVPATH 503
+// Path does not exist
+#define EPNOEXIST 504
+
+#endif
--- kernel/src/kernel/cpio.c
+++ kernel/src/kernel/cpio.c
@@ -1,4 +1,5 @@
 #include <kernel/cpio.h>
+#include <kernel/error.h>
 #include <kernel/serial.h>
 #include <liballoc/liballoc.h>
 #include <memory.h>
