dac60c29f817b7089d3134fd6739ad5e65fafc41
ryuukumar <adityakumar4644@gmail.com>
2026-03-20 14:29:03 +0530

    Apply formatting

--- kernel/include/kernel/elf.h
+++ kernel/include/kernel/elf.h
@@ -5,26 +5,26 @@
 #include <stdint.h>
 
 typedef struct __attribute__ ((packed)) {
-	char elf_magic[4];
-	char elf_class[1];
-	char elf_data[1];
-	uint8_t elf_vern1;
-	char elf_osabi[1];
-	char elf_osabivern[1];
-	char elf_zero[7];
+	char	 elf_magic[4];
+	char	 elf_class[1];
+	char	 elf_data[1];
+	uint8_t	 elf_vern1;
+	char	 elf_osabi[1];
+	char	 elf_osabivern[1];
+	char	 elf_zero[7];
 	uint16_t elf_type;
 	uint16_t elf_iset;
 	uint32_t elf_vern2;
 } elf_scanner_t;
 
 typedef struct __attribute__ ((packed)) {
-	char elf_magic[4];
-	char elf_class[1];
-	char elf_data[1];
-	uint8_t elf_vern1;
-	char elf_osabi[1];
-	char elf_osabivern[1];
-	char elf_zero[7];
+	char	 elf_magic[4];
+	char	 elf_class[1];
+	char	 elf_data[1];
+	uint8_t	 elf_vern1;
+	char	 elf_osabi[1];
+	char	 elf_osabivern[1];
+	char	 elf_zero[7];
 	uint16_t elf_type;
 	uint16_t elf_iset;
 	uint32_t elf_vern2;
