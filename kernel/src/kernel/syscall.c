68d3c0d120ba3befa88c437cc2d9e8aa75320c5d
ryuukumar <adityakumar4644@gmail.com>
2026-03-21 00:16:25 +0530

    Add a really simple sys_fork

--- kernel/src/kernel/syscall.c
+++ kernel/src/kernel/syscall.c
@@ -1,8 +1,22 @@
+#include <kernel/process.h>
 #include <kernel/serial.h>
 #include <kernel/syscall.h>
 
 registers_t* syscall_handler (registers_t* registers) {
-	// uint8_t syscall_number = registers->rax;
+	uint64_t syscall_number = registers->rax;
+
+	process* current = get_current_process ();
+	if (current) current->p_registers_ptr = registers;
+
+	if (syscall_number == 57) { // sys_fork
+		process* child = NULL;
+		int		 status = process_fork (current, &child);
+		if (status == 0 && child != NULL)
+			registers->rax = child->p_id;
+		else
+			registers->rax = (uint64_t)-1;
+	}
+
 	return registers;
 }
 
