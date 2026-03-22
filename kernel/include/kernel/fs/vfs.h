90edc3c62da27edbcfa3d82e39fe399e7c6626c0
ryuukumar <adityakumar4644@gmail.com>
2026-03-22 16:01:39 +0530

    Format

--- kernel/include/kernel/fs/vfs.h
+++ kernel/include/kernel/fs/vfs.h
@@ -8,8 +8,8 @@
 
 #define O_RDONLY 0x0000
 #define O_WRONLY 0x0001
-#define O_RDWR   0x0002
-#define O_CREAT  0x0040
+#define O_RDWR	 0x0002
+#define O_CREAT	 0x0040
 
 #define SEEK_SET 0
 #define SEEK_CUR 1
