beed184182f86b3c9a0a51d4ce613b3f5eca46a1
ryuukumar <adityakumar4644@gmail.com>
2026-03-22 12:07:53 +0530

    Update hello.c to our syscall conventions

--- user/hello/hello.c
+++ user/hello/hello.c
@@ -5,15 +5,15 @@ static inline long syscall3 (long num, long arg1, long arg2, long arg3) {
 	long ret;
 	__asm__ volatile ("int $0x80"
 					  : "=a"(ret)
-					  : "a"(num), "b"(arg1), "c"(arg2), "d"(arg3)
+					  : "a"(num), "D"(arg1), "S"(arg2), "d"(arg3) // rdi, rsi, rdx
 					  : "memory");
 	return ret;
 }
 
 void _start (void) {
-	const char* msg = "Hello world!\n";
+	const char* msg = "Hello from USERLAND!!!\n";
 
-	syscall3 (4, 1, (long)msg, 13);
+	syscall3 (4, 1, (long)msg, 24);
 	syscall3 (1, 0, 0, 0);
 
 	while (1)
