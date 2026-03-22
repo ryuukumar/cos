d37c60e6a2bb76a2f483f260ce1c9f484b235ab4
ryuukumar <adityakumar4644@gmail.com>
2026-03-22 14:17:49 +0530

    Route write syscall

--- kernel/include/kernel/fs/vfs.h
+++ kernel/include/kernel/fs/vfs.h
@@ -52,10 +52,12 @@ int do_lookup (char* filename, inode** result, inode* root);
 
 int do_read (struct file* f, void* buf, size_t size);
 int do_seek (struct file* f, size_t offset, int whence);
+int do_write (struct file* f, void* buf, size_t size);
 int do_open (inode* file, struct file* dest_fd);
 int do_close (struct file* fd);
 
 uint64_t sys_read (uint64_t fd, uint64_t buf, uint64_t size);
+uint64_t sys_write (uint64_t fd, uint64_t buf, uint64_t size);
 uint64_t sys_seek (uint64_t fd, uint64_t offset, uint64_t whence);
 uint64_t sys_open (uint64_t filename_ptr, uint64_t flags, uint64_t mode);
 uint64_t sys_close (uint64_t fd, uint64_t arg2, uint64_t arg3);
