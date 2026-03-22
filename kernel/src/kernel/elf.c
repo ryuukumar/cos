fbed09cc324ebd9d97e6d082be3eae41a7388cf3
ryuukumar <adityakumar4644@gmail.com>
2026-03-22 12:53:35 +0530

    Register vfs endpoints as syscalls

--- kernel/src/kernel/elf.c
+++ kernel/src/kernel/elf.c
@@ -2,6 +2,7 @@
 #include <kernel/error.h>
 #include <kernel/fs/vfs.h>
 #include <kernel/memmgt.h>
+#include <kernel/syscall.h>
 #include <liballoc/liballoc.h>
 #include <memory.h>
 #include <stdbool.h>
@@ -33,13 +34,15 @@ bool verify_elf_loadable (elf64_header_t* elf) {
 
 int load_elf (const char* filepath, process* target_process, uintptr_t* entry_point_r) {
 	// TODO: open file as readonly once flags implemented
-	int fd = sys_open (filepath, 0, 0);
+	int64_t fd = (int64_t)do_syscall (SYSCALL_SYS_OPEN, (uint64_t)filepath, 0ull, 0ull);
 	if (fd < 0) return fd;
 
 	elf64_header_t elf_header;
-	int			   bytes_read = sys_read (fd, &elf_header, sizeof (elf64_header_t));
+
+	int64_t bytes_read = (int64_t)do_syscall (SYSCALL_SYS_READ, (uint64_t)fd, (uint64_t)&elf_header,
+								 (uint64_t)sizeof (elf64_header_t));
 	if (bytes_read != sizeof (elf64_header_t) || !verify_elf_loadable (&elf_header)) {
-		sys_close (fd);
+		do_syscall (SYSCALL_SYS_CLOSE, fd, 0, 0);
 		return -ENOEXEC;
 	}
 
@@ -49,8 +52,8 @@ int load_elf (const char* filepath, process* target_process, uintptr_t* entry_po
 	size_t			 ph_size = elf_header.elf_phnum * elf_header.elf_phentsize;
 	elf64_pheader_t* program_headers = kmalloc (ph_size);
 
-	sys_seek (fd, elf_header.elf_phoff, SEEK_SET);
-	sys_read (fd, program_headers, ph_size);
+	do_syscall (SYSCALL_SYS_LSEEK, fd, elf_header.elf_phoff, SEEK_SET);
+	do_syscall (SYSCALL_SYS_READ, fd, (uint64_t)program_headers, ph_size);
 
 	for (int i = 0; i < elf_header.elf_phnum; i++) {
 		elf64_pheader_t* ph = &program_headers[i];
@@ -65,15 +68,15 @@ int load_elf (const char* filepath, process* target_process, uintptr_t* entry_po
 
 		alloc_by_cr3 (target_process->p_cr3, (uintptr_t)start, num_pages, is_writable);
 
-		sys_seek (fd, ph->p_offset, SEEK_SET);
-		sys_read (fd, (void*)ph->p_vaddr, ph->p_filesz);
+		do_syscall (SYSCALL_SYS_LSEEK, fd, ph->p_offset, SEEK_SET);
+		do_syscall (SYSCALL_SYS_READ, fd, ph->p_vaddr, ph->p_filesz);
 
 		if (ph->p_memsz > ph->p_filesz)
 			memset ((void*)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 	}
 
 	kfree (program_headers);
-	sys_close (fd);
+	do_syscall (SYSCALL_SYS_CLOSE, fd, 0, 0);
 
 	*entry_point_r = elf_header.elf_entry;
 	return 0;
