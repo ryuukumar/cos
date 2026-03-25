75107d9e81f2970a3a8c59c9801a9f81715fe2b4
ryuukumar <adityakumar4644@gmail.com>
2026-03-25 13:00:54 +0530

    Add docs

--- kernel/include/kassert.h
+++ kernel/include/kassert.h
@@ -4,9 +4,10 @@
 typedef void (*kassert_handler) (bool, const char*);
 
 void kassert_halt (bool condition);
-void kassert_handle (bool condition, kassert_handler handler);
+void kassert_handle (bool condition, const char* message, kassert_handler handler);
 
 // Requires serial to be initialised!
+
 void kassert_print_halt (bool condition, const char* message);
 void kassert_print_no_block (bool condition, const char* message);
 void kassert_print_handle (bool condition, const char* message, kassert_handler handler);
--- kernel/src/kassert.c
+++ kernel/src/kassert.c
@@ -1,19 +1,32 @@
 #include <kassert.h>
 #include <kernel/serial.h>
 
-constexpr char empty_str[] = "";
-
+/*!
+ * Assert condition is true, if not, then busy wait
+ * @param condition condition to be checked
+ */
 inline void kassert_halt (bool condition) {
 	if (condition) return;
 	for (;;)
 		;
 }
 
-inline void kassert_handle (bool condition, kassert_handler handler) {
+/*!
+ * Assert condition is true, if not, then forward to handler
+ * @param condition condition to be checked
+ * @param message message to be forwarded to handler
+ * @param handler handler to forward to
+ */
+inline void kassert_handle (bool condition, const char* message, kassert_handler handler) {
 	if (condition) return;
-	handler (condition, (const char*)&empty_str);
+	handler (condition, message);
 }
 
+/*!
+ * Assert condition is true, if not, then print to serial and busy wait
+ * @param condition condition to be checked
+ * @param message message to be printed
+ */
 inline void kassert_print_halt (bool condition, const char* message) {
 	if (condition) return;
 	write_serial_str ("KASSERT (halt) failed for:\n");
@@ -22,12 +35,23 @@ inline void kassert_print_halt (bool condition, const char* message) {
 		;
 }
 
+/*!
+ * Assert condition is true, if not, then print to serial and continue execution
+ * @param condition condition to be checked
+ * @param message message to be printed
+ */
 inline void kassert_print_no_block (bool condition, const char* message) {
 	if (condition) return;
 	write_serial_str ("KASSERT (non-block) failed for:\n");
 	write_serial_str (message);
 }
 
+/*!
+ * Assert condition is true, if not, then print to serial and forward to handler
+ * @param condition condition to be checked
+ * @param message message to be printed and forwarded to handler
+ * @param handler handler to forward to
+ */
 inline void kassert_print_handle (bool condition, const char* message, kassert_handler handler) {
 	if (condition) return;
 	write_serial_str ("KASSERT (handle) failed for:\n");
