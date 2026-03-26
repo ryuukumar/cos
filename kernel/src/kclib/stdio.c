46c64777b144693ff05bbf3af5d4b1e0e53da7b6
ryuukumar <adityakumar4644@gmail.com>
2026-03-26 15:21:12 +0530

    Add new format_handler and kv_core_printf

--- kernel/src/kclib/stdio.c
+++ kernel/src/kclib/stdio.c
@@ -1,82 +1,99 @@
+#include <kclib/memory.h>
 #include <kclib/stdio.h>
 #include <kclib/string.h>
 #include <kernel/console.h>
+#include <kernel/serial.h>
+#include <liballoc/liballoc.h>
 
 /*!
-Handle the different cases of %-- in printf
+ * Handle the next % case in printf. Currently handles: d,i -> signed int, u -> unsigned int, x,X ->
+ * unsigned hexadecimal (case not considered), l,ll prefix -> 64-bit, h prefix -> 16-bit, hh prefix
+ * -> 8-bit, s -> string, % -> actual % sign
+ * @param input full input fmt string
+ * @param idx pointer to index at which % was encountered
+ * @param list argument list
+ * @param r_output pointer to char* which will be populated with output (will be mallocd)
+ * @return number of bytes read from input
+ */
+static size_t format_handler (const char* input, size_t idx, va_list* list, char** r_output) {
+	if (!input || input[idx] != '%') {
+		*r_output = nullptr;
+		return 0;
+	}
 
-@param	format the formatted string to be printed
-@param	i	pointer to the index we are at
-@param	args	pointer to the arguments printf received
-@param	ul	whether the format prints an unsigned/long character
-*/
-static void kfmtprintf (const char* format, size_t* i, va_list* args, bool ul) {
-	switch (format[*i]) {
-	case 's': {
-		const char* str = va_arg (*args, const char*);
-		putstr (str, kstrlen (str));
-	} break;
+	size_t consumed = 1; /* '%' */
+	size_t input_bytes = 32;
+
+	idx++;
+	while (input[idx] == 'l' || input[idx] == 'h') {
+		if (input[idx] == 'l')
+			input_bytes = 64;
+		else
+			input_bytes /= 2;
+		idx++;
+		consumed++;
+	}
+
+	char spec = input[idx++];
+	consumed++;
+
+	char* buf = NULL;
+	switch (spec) {
+	case 's':
+		buf = kstrdup (va_arg (*list, const char*));
+		break;
+	case '%':
+		buf = kmalloc (2);
+		buf[0] = '%';
+		buf[1] = '\0';
+		break;
 	case 'd':
-	case 'i': {
-		if (ul) {
-			char buf[65] = {0};
-			kulitos (va_arg (*args, uint64_t), buf, 10);
-			putstr (buf, kstrlen (buf));
+	case 'i':
+		if (input_bytes == 64) {
+			buf = kmalloc (65);
+			kulitos (va_arg (*list, uint64_t), buf, 10);
 		} else {
-			char buf[33] = {0};
-			kitos (va_arg (*args, int32_t), buf, 10);
-			putstr (buf, kstrlen (buf));
+			buf = kmalloc (33);
+			kitos (va_arg (*list, int32_t), buf, 10);
 		}
-	} break;
-	case 'x': {
-		if (ul) {
-			char buf[65] = {0};
-			kulitos (va_arg (*args, uint64_t), buf, 16);
-			putstr (buf, kstrlen (buf));
-		} else {
-			char buf[33] = {0};
-			kitos (va_arg (*args, int32_t), buf, 16);
-			putstr (buf, kstrlen (buf));
-		}
-	} break;
+		break;
 	case 'u':
-	case 'l': {
-		(*i)++;
-		kfmtprintf (format, i, args, true);
-	} break;
-	default: {
-		putchar ('%');
-		putchar (format[*i]);
+		if (input_bytes == 64) {
+			buf = kmalloc (65);
+			kulitos (va_arg (*list, uint64_t), buf, 10);
+		} else {
+			buf = kmalloc (33);
+			kulitos (va_arg (*list, uint32_t), buf, 10);
+		}
+		break;
+	case 'x':
+	case 'X':
+		if (input_bytes == 64) {
+			buf = kmalloc (17);
+			kulitos (va_arg (*list, uint64_t), buf, 16);
+		} else {
+			buf = kmalloc (9);
+			kulitos (va_arg (*list, uint32_t), buf, 16);
+		}
+		break;
+	default:
+		*r_output = nullptr;
+		return consumed;
 	}
-	}
+
+	*r_output = buf;
+	return consumed;
 }
 
-/*!
-Print a formatted string to the screen.
-
-@param	format formatted string to print
-*/
-void kprintf (const char* format, ...) {
-	va_list args;
-	va_start (args, format);
-
-	bool buf = get_update_on_putch ();
-	set_update_on_putch (false);
-	for (size_t i = 0; i < kstrlen (format); i++) {
-		switch (format[i]) {
-		case '%': {
-			i++;
-			kfmtprintf (format, &i, &args, false);
-		} break;
-		case '\t':
-			size_t idx = get_idx ();
-			idx = (idx / TAB_WIDTH + 1) * TAB_WIDTH;
-			set_idx (idx);
-			break;
-		default:
-			putchar (format[i]);
-		}
+static void kv_core_printf (const char* fmt, va_list* list, void (*putchar_call) (unsigned char)) {
+	char* tmpbuf = nullptr;
+	for (size_t input_idx = 0; input_idx < kstrlen (fmt);) {
+		if (fmt[input_idx] == '%') {
+			input_idx += format_handler (fmt, input_idx, list, &tmpbuf);
+			for (size_t tmp_idx = 0; tmp_idx < kstrlen (tmpbuf); tmp_idx++)
+				putchar_call (tmpbuf[tmp_idx]);
+			kfree (tmpbuf), tmpbuf = nullptr;
+		} else
+			putchar_call (fmt[input_idx++]);
 	}
-	update ();
-	set_update_on_putch (buf);
 }
