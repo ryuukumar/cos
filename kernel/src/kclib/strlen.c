6a7b94422df3e8fe28373b18005b9580b584852e
ryuukumar <adityakumar4644@gmail.com>
2026-03-27 20:45:56 +0530

    Actuall implement strlen functions properly

--- kernel/src/kclib/strlen.c
+++ kernel/src/kclib/strlen.c
@@ -19,9 +19,22 @@ size_t kstrlen (const char* s) {
  * @param strsz max length to scan
  * @return min of string size and strsz
  */
-size_t kstrnlen_s (const char* s, size_t strsz) {
+size_t kstrnlen (const char* s, size_t n) {
+	if (s == nullptr) return 0;
 	size_t ret = 0;
-	while (ret < strsz && s[ret] != 0)
+	while (ret < n && s[ret] != 0)
 		ret++;
 	return ret;
+}
+
+/*!
+ * Get the length of a standard string (terminating with 0). Stops search at strsz, if 0 character
+ * is not yet encountered.
+ * @param s string to check
+ * @param strsz max length to scan
+ * @return min of string size and strsz
+ */
+size_t kstrnlen_s (const char* s, size_t strsz) {
+	if (s == nullptr) return 0;
+	return kstrnlen (s, strsz);
 }
