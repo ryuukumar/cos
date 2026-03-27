15b581bbab08ccad6995a8dce44e89d3d62dcad0
ryuukumar <adityakumar4644@gmail.com>
2026-03-27 20:27:37 +0530

    Format + add safe strlen

--- kernel/include/kclib/string.h
+++ kernel/include/kclib/string.h
@@ -5,45 +5,47 @@
 #include <stddef.h>
 #include <stdint.h>
 
-void* kmemcpy(void* restrict s1, const void* restrict s2, size_t n);
-void* kmemccpy(void* restrict s1, const void* restrict s2, int c, size_t n);
-void* kmemmove(void* s1, const void* s2, size_t n);
-char* kstrcpy(char* restrict s1, const char* restrict s2);
-char* kstrncpy(char* restrict s1, const char* restrict s2, size_t n);
-char* kstrdup(const char* s);
-char* kstrndup(const char* s, size_t n);
-char* kstrcat(char* restrict s1, const char* restrict s2);
-char* kstrncat(char* restrict s1, const char* restrict s2, size_t n);
-int kmemcmp(const void* s1, const void* s2, size_t n);
-int kstrcmp(const char* s1, const char* s2);
-int kstrcoll(const char* s1, const char* s2);
-int kstrncmp(const char* s1, const char* s2, size_t n);
-size_t kstrxfrm(char* restrict s1, const char* restrict s2, size_t n);
-void* kmemchr(void* s, int c, size_t n);
-char* kstrchr(char* s, int c);
-size_t kstrcspn(const char* s1, const char* s2);
-char* kstrpbrk(char* s1, const char* s2);
-char* kstrrchr(char* s, int c);
-size_t kstrspn(const char* s1, const char* s2);
-char* kstrstr(char* s1, const char* s2);
-char* kstrtok(char* restrict s1, const char* restrict s2);
-void* kmemset(void* s, int c, size_t n);
-void* kmemset_explicit(void* s, int c, size_t n);
-char* kstrerror(int errnum);
-size_t kstrlen(const char* s);
-size_t kstrnlen(const char* s, size_t n);
+void*  kmemcpy (void* restrict s1, const void* restrict s2, size_t n);
+void*  kmemccpy (void* restrict s1, const void* restrict s2, int c, size_t n);
+void*  kmemmove (void* s1, const void* s2, size_t n);
+char*  kstrcpy (char* restrict s1, const char* restrict s2);
+char*  kstrncpy (char* restrict s1, const char* restrict s2, size_t n);
+char*  kstrdup (const char* s);
+char*  kstrndup (const char* s, size_t n);
+char*  kstrcat (char* restrict s1, const char* restrict s2);
+char*  kstrncat (char* restrict s1, const char* restrict s2, size_t n);
+int	   kmemcmp (const void* s1, const void* s2, size_t n);
+int	   kstrcmp (const char* s1, const char* s2);
+int	   kstrcoll (const char* s1, const char* s2);
+int	   kstrncmp (const char* s1, const char* s2, size_t n);
+size_t kstrxfrm (char* restrict s1, const char* restrict s2, size_t n);
+void*  kmemchr (void* s, int c, size_t n);
+char*  kstrchr (char* s, int c);
+size_t kstrcspn (const char* s1, const char* s2);
+char*  kstrpbrk (char* s1, const char* s2);
+char*  kstrrchr (char* s, int c);
+size_t kstrspn (const char* s1, const char* s2);
+char*  kstrstr (char* s1, const char* s2);
+char*  kstrtok (char* restrict s1, const char* restrict s2);
+void*  kmemset (void* s, int c, size_t n);
+void*  kmemset_explicit (void* s, int c, size_t n);
+char*  kstrerror (int errnum);
+size_t kstrlen (const char* s);
+size_t kstrnlen (const char* s, size_t n);
 
-void* kmemcpy_s(void* restrict s1, rsize_t s1max, const void* restrict s2, rsize_t n);
-void* kmemmove_s(void* s1, rsize_t s1max, const void* s2, rsize_t n);
-char* kstrcpy_s(char* restrict s1, rsize_t s1max, const char* restrict s2);
-char* kstrncpy_s(char* restrict s1, rsize_t s1max, const char* restrict s2, rsize_t n);
-char* kstrcat_s(char* restrict s1, rsize_t s1max, const char* restrict s2);
-char* kstrncat_s(char* restrict s1, rsize_t s1max, const char* restrict s2, rsize_t n);
-char* kstrtok_s(char* restrict s1, rsize_t* restrict s1max,
-               const char* restrict s2, char** restrict ptr);
-void* kmemset_s(void* s, rsize_t smax, int c, rsize_t n)
-char* kstrerror_s(char* s, rsize_t maxsize, errno_t errnum);
-size_t kstrerrorlen_s(errno_t errnum);
-size_t kstrnlen_s(const char* s, size_t maxsize);
+
+size_t kstrlen_s (const char* s, size_t strsz);
+void*  kmemcpy_s (void* restrict s1, size_t s1max, const void* restrict s2, size_t n);
+void*  kmemmove_s (void* s1, size_t s1max, const void* s2, size_t n);
+char*  kstrcpy_s (char* restrict s1, size_t s1max, const char* restrict s2);
+char*  kstrncpy_s (char* restrict s1, size_t s1max, const char* restrict s2, size_t n);
+char*  kstrcat_s (char* restrict s1, size_t s1max, const char* restrict s2);
+char*  kstrncat_s (char* restrict s1, size_t s1max, const char* restrict s2, size_t n);
+char*  kstrtok_s (char* restrict s1, size_t* restrict s1max, const char* restrict s2,
+				  char** restrict ptr);
+void*  kmemset_s (void* s, size_t smax, int c,
+				  size_t n) char* kstrerror_s (char* s, size_t maxsize, errno_t errnum);
+size_t kstrerrorlen_s (errno_t errnum);
+size_t kstrnlen_s (const char* s, size_t maxsize);
 
 #endif
