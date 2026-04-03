3d4a1c007e21e87f895a95372ae220a378ec9753
ryuukumar <adityakumar4644@gmail.com>
2026-04-03 12:10:29 +0530

    Add expected spec for charqueue

---
+++ kernel/include/utils/charqueue.h
@@ -0,0 +1,29 @@
+#ifndef CHARQUEUE_H
+#define CHARQUEUE_H
+
+#include <kernel/memmgt.h>
+
+typedef struct charqueue_page_t charqueue_page_t;
+
+struct charqueue_page_t {
+	charqueue_page_t* next;
+	unsigned char	  data[PAGE_SIZE - sizeof (charqueue_page_t*)];
+};
+
+typedef struct {
+	charqueue_page_t* current_page;
+	uint64_t		  offset;
+} charqueue_ptr_t;
+
+typedef struct {
+	charqueue_ptr_t head, tail;
+} charqueue;
+
+charqueue* create_charqueue (void);
+int		   free_charqueue (charqueue* queue);
+
+int push_charqueue (charqueue* queue, unsigned char insert);
+int pop_charqueue (charqueue* queue, unsigned char* ret);
+int peek_charqueue (charqueue* queue, unsigned char* ret);
+
+#endif
