84d1a238a343ab8ef5a3fb24c115444301cc554f
ryuukumar <adityakumar4644@gmail.com>
2026-04-03 12:31:11 +0530

    Allow interrupt state manipulation using flags

--- kernel/src/kernel/io.c
+++ kernel/src/kernel/io.c
@@ -25,3 +25,30 @@ inline uint8_t inb (uint16_t port) {
  * Short wait (1-4 ms)
  */
 inline void io_wait (void) { outb (0x80, 0); }
+
+/*!
+ * Get flags and clear interrupt flag.
+ * @return flags before the interrupt flag was cleared
+ */
+inline uint64_t save_irq_disable (void) {
+	uint64_t flags;
+	__asm__ volatile ("pushfq\n\t"
+					  "pop %0\n\t"
+					  "cli"
+					  : "=rm"(flags)
+					  :
+					  : "memory");
+	return flags;
+}
+
+/*!
+ * Restore flags.
+ * @param flags flags to set (typically return value of save_irq_disable)
+ */
+inline void restore_irq (uint64_t flags) {
+	__asm__ volatile ("push %0\n\t"
+					  "popfq"
+					  :
+					  : "rm"(flags)
+					  : "memory");
+}
