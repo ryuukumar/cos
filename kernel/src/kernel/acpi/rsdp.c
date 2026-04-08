5989566d2b445c2f3eb2ced3349c92b1c91e1dfd
ryuukumar <adityakumar4644@gmail.com>
2026-04-08 19:49:24 +0530

    Add init_and_return and some extra verification/fixes

--- kernel/src/kernel/acpi/rsdp.c
+++ kernel/src/kernel/acpi/rsdp.c
@@ -1,9 +1,17 @@
+#include <kclib/string.h>
 #include <kernel/acpi/rsdp.h>
 #include <kernel/memmgt.h>
+
+static bool is_init = false;
 
 static RSDP_t* rsdp_ptr = nullptr;
 static XSDP_t* xsdp_ptr = nullptr;
 static uint8_t rsdp_ver = 0;
+
+static void* init_and_return (void* arg) {
+	is_init = true;
+	return arg;
+}
 
 uint8_t get_rsdp_revision () { return rsdp_ver; }
 
@@ -19,6 +27,7 @@ void* init_rsdp (uintptr_t rsdp_base_ptr, uint64_t hhdm_offset) {
 			(void*)ALIGN_PAGE_DOWN (rsdp_base_ptr + hhdm_offset + sizeof (XSDP_t)));
 
 		alloc_all_vpages_in_range (rsdp_vaddr_start, rsdp_vaddr_end, rsdp_base_frame);
+		rsdp_base_ptr += hhdm_offset;
 	}
 
 	RSDP_t* tmp_rsdp_ptr = (RSDP_t*)rsdp_base_ptr;
@@ -29,16 +38,17 @@ void* init_rsdp (uintptr_t rsdp_base_ptr, uint64_t hhdm_offset) {
 	for (uint64_t i = 0; i < sizeof (RSDP_t); i++)
 		rsdp_checksum += ((uint8_t*)tmp_rsdp_ptr)[i];
 	if (rsdp_checksum != 0) return nullptr;
+	if (kmemcmp (tmp_rsdp_ptr->signature, "RSD PTR ", 8) != 0) return nullptr;
 	rsdp_ver = tmp_rsdp_ptr->revision;
 
 	// if we are on rev 1, set rsdp_ptr and leave
-	if (rsdp_ver == 1) return rsdp_ptr = tmp_rsdp_ptr;
+	if (rsdp_ver == 0) return init_and_return (rsdp_ptr = tmp_rsdp_ptr);
 
 	// validate the XSDP
 	uint8_t xsdp_checksum = 0;
-	for (uint64_t j = sizeof (RSDP_t); j < sizeof (XSDP_t); j++)
+	for (uint64_t j = 0; j < sizeof (XSDP_t); j++)
 		xsdp_checksum += ((uint8_t*)tmp_xsdp_ptr)[j];
 	if (xsdp_checksum != 0) return nullptr;
 
-	return xsdp_ptr = tmp_xsdp_ptr;
+	return init_and_return (xsdp_ptr = tmp_xsdp_ptr);
 }
