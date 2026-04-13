472d2b3a5861101dee1f57c9c62e5d28058e4900
ryuukumar <adityakumar4644@gmail.com>
2026-04-13 19:48:20 +0530

    Fix header for RSDT

--- kernel/include/kernel/acpi/rsdt.h
+++ kernel/include/kernel/acpi/rsdt.h
@@ -1,13 +1,14 @@
 #ifndef ACPI_RSDT_H
 #define ACPI_RSDT_H
 
-#include <kernel/acpi/sdt_header.h>
+#include <kernel/acpi/acpi_common.h>
+#include <kernel/acpi/rsdp.h>
 
 typedef struct __attribute__ ((packed)) {
 	SDT_header_t header;
 	uint32_t	 other_sdt_ptrs[];
 } RSDT_t;
 
-void init_rsdt (RSDP_t* rsdp_base_ptr);
+void init_rsdt (uint32_t rsdt_base_ptr, uint64_t hhdm_offset);
 
 #endif
