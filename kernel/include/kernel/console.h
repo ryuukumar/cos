937abc1f034c1a92a599b731df09aa3fd5f7f48e
ryuukumar <adityakumar4644@gmail.com>
2026-04-26 09:21:47 +0530

    Add union for console char type

--- kernel/include/kernel/console.h
+++ kernel/include/kernel/console.h
@@ -6,6 +6,17 @@
 
 #define TAB_WIDTH 8
 
+typedef union {
+	uint32_t raw;
+	struct __attribute__((packed)) {
+		uint8_t		  dirty : 1;
+		unsigned char character : 7;
+		uint8_t		  red;
+		uint8_t		  green;
+		uint8_t		  blue;
+	};
+} console_char_t;
+
 void   set_update_on_putch (bool);
 bool   get_update_on_putch (void);
 void   set_idx (size_t);
