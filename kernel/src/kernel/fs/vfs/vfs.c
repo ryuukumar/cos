41a057ccb8f304c079057a3dd70f211a4d575ff0
ryuukumar <adityakumar4644@gmail.com>
2026-04-26 11:16:53 +0530

    Isolate vfs/read

--- kernel/src/kernel/fs/vfs/vfs.c
+++ kernel/src/kernel/fs/vfs/vfs.c
@@ -228,36 +228,10 @@ int do_lookup (char* filename, inode** result, inode* root) {
 	return do_lookup (next_slash, result, target_inode);
 }
 
-/*!
- * Execute the read routine.
- * @param f pointer to file structure to read via
- * @param buf pointer to allocated memory to write to
- * @param size desired read size
- * @return actual bytes read if successful, error (<0) otherwise
- */
-int do_read (struct file* f, void* buf, size_t size) {
-	if (!f || !buf) return -EINVARG;
-	if (!f->f_fops || !f->f_fops->read) return -ENOIMPL;
-	return f->f_fops->read (f->f_inode, f, buf, size);
-}
-
 int do_seek (struct file* f, size_t offset, int whence) {
 	if (!f || whence >= 3 || whence < 0) return -EINVARG;
 	if (!f->f_fops || !f->f_fops->seek) return -ENOIMPL;
 	return f->f_fops->seek (f->f_inode, f, offset, whence);
-}
-
-/*!
- * Read upto `size` bytes of an opened file.
- * @param fd file descriptor of file to read
- * @param buf pointer to allocated memory to write to
- * @param size desired read size
- * @return actual bytes read if successful, error (<0) otherwise
- */
-uint64_t sys_read (uint64_t fd, uint64_t buf, uint64_t size) {
-	process* current = get_current_process ();
-	if (fd >= MAX_FDS || !current || !current->p_fds[fd]) return -EINVARG;
-	return do_read (current->p_fds[fd], (void*)buf, (size_t)size);
 }
 
 /*!
