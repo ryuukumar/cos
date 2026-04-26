cf3376e73a671273d084d03f944dc390e8af6272
ryuukumar <adityakumar4644@gmail.com>
2026-04-26 11:17:47 +0530

    Isolate vfs/seek

--- kernel/src/kernel/fs/vfs/vfs.c
+++ kernel/src/kernel/fs/vfs/vfs.c
@@ -228,26 +228,6 @@ int do_lookup (char* filename, inode** result, inode* root) {
 	return do_lookup (next_slash, result, target_inode);
 }
 
-int do_seek (struct file* f, size_t offset, int whence) {
-	if (!f || whence >= 3 || whence < 0) return -EINVARG;
-	if (!f->f_fops || !f->f_fops->seek) return -ENOIMPL;
-	return f->f_fops->seek (f->f_inode, f, offset, whence);
-}
-
-/*!
- * Seek to an offset in an open file. Behavior depends on whence: SEEK_SET -> seek exactly offset,
- * SEEK_CUR -> seek current offset plus given offset, SEEK_END -> seek end of file plus given offset
- * @param fd file descriptor
- * @param offset offset value
- * @param whence SEEK_SET, SEEK_CUR or SEEK_END
- * @return 0 if successful, else error
- */
-uint64_t sys_seek (uint64_t fd, uint64_t offset, uint64_t whence) {
-	process* current = get_current_process ();
-	if (fd >= MAX_FDS || !current || !current->p_fds[fd]) return -EINVARG;
-	return do_seek (current->p_fds[fd], (size_t)offset, (int)whence);
-}
-
 inode* get_absolute_root (void) { return vfs_absolute_root; }
 
 void init_vfs (inode* absolute_root) {
