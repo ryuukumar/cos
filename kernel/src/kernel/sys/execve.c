9c98f352e3b32267357591b2e2a0ccdf782fbcc6
ryuukumar <adityakumar4644@gmail.com>
2026-04-27 17:23:28 +0530

    Properly cleanup in execve

--- kernel/src/kernel/sys/execve.c
+++ kernel/src/kernel/sys/execve.c
@@ -1,5 +1,7 @@
+#include <kclib/stdio.h>
 #include <kclib/string.h>
 #include <kernel/elf.h>
+#include <kernel/error.h>
 #include <kernel/exec.h>
 #include <kernel/memmgt.h>
 #include <kernel/process.h>
@@ -16,20 +18,29 @@ static char** arg_deep_copy (char* const arg_og[]) {
 	char** arg_new = kmalloc ((count + 1) * sizeof (char*));
 	if (!arg_new) return nullptr;
 
-	kmemcpy ((void*)arg_new, (void*)arg_og, count);
+	for (int i = 0; i < count; i++)
+		arg_new[i] = kstrdup (arg_og[i]);
+
 	arg_new[count] = nullptr;
 
 	return arg_new;
 }
 
 static int do_execve (const char* path, char* const argv[], char* const envp[]) {
+	int err = 0;
+
 	const char* path_cp = kstrdup (path);
 	char**		argv_cp = arg_deep_copy (argv);
 	char**		envp_cp = arg_deep_copy (envp);
 
+	if (!path_cp || !argv_cp || !envp_cp) {
+		err = -ENOMEM;
+		goto cleanup_envc_argc_path;
+	}
+
 	uintptr_t entry_point;
-	int		  err = load_elf (path_cp, get_current_process (), &entry_point);
-	if (err != 0) return err;
+	err = load_elf (path_cp, get_current_process (), &entry_point);
+	if (err != 0) goto cleanup_envc_argc_path;
 
 	vaddr_t	  us_base_vaddr = {254, 255, 0, 0, 0};
 	uintptr_t user_stack_base = (uintptr_t)vaddr_t_to_ptr (&us_base_vaddr);
@@ -46,6 +57,11 @@ static int do_execve (const char* path, char* const argv[], char* const envp[]) 
 	uintptr_t* argv_ptrs = kmalloc (argc * sizeof (uintptr_t));
 	uintptr_t* envp_ptrs = kmalloc (envc * sizeof (uintptr_t));
 	uintptr_t  sp = user_stack_base;
+
+	if (!argv_ptrs || !envp_ptrs) {
+		err = -ENOMEM;
+		goto cleanup_all;
+	}
 
 	for (int i = envc - 1; i >= 0; i--) {
 		size_t len = kstrlen (envp_cp[i]) + 1;
@@ -83,21 +99,27 @@ static int do_execve (const char* path, char* const argv[], char* const envp[]) 
 
 	sp -= 8;
 	*((uintptr_t*)sp) = argc;
-
-	kfree (argv_ptrs);
-	kfree (envp_ptrs);
-
-	for (int i = 0; i < argc; i++)
-		kfree ((void*)argv_cp[i]);
-	kfree ((void*)argv_cp);
-
-	for (int i = 0; i < envc; i++)
-		kfree ((void*)envp_cp[i]);
-	kfree ((void*)envp_cp);
-
 	get_current_process ()->p_registers_ptr->rsp = sp;
 	get_current_process ()->p_registers_ptr->rip = entry_point;
 
+cleanup_all:
+	if (envp_ptrs) kfree (envp_ptrs);
+	if (argv_ptrs) kfree (argv_ptrs);
+
+cleanup_envc_argc_path:
+	if (envp_cp) {
+		for (int i = 0; i < envc; i++)
+			kfree ((void*)envp_cp[i]);
+		kfree ((void*)envp_cp);
+	}
+
+	if (argv_cp) {
+		for (int i = 0; i < argc; i++)
+			kfree ((void*)argv_cp[i]);
+		kfree ((void*)argv_cp);
+	}
+
+	kfree ((void*)path_cp);
 	return 0;
 }
 
