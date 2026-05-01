9f04bca495b9a78f9b1aeafb2d90ce44e7f3b0cb
ryuukumar <adityakumar4644@gmail.com>
2026-05-01 22:35:05 +0530

    Update stubs

--- lib/cos/src/stubs.c
+++ lib/cos/src/stubs.c
@@ -1,18 +1,27 @@
 #include <errno.h>
+#include <stdarg.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 #undef errno
 extern int errno;
 
+// declaration
+int fcntl (int fd, int cmd, ...);
+
 int fstat (int file, struct stat* st) {
 	st->st_mode = S_IFCHR;
 	return 0;
 }
 
-int isatty (int file) { return 1; }
-
 int kill (int pid, int sig) {
 	errno = EINVAL;
+	return -1;
+}
+
+int fcntl (int fd, int cmd, ...) {
+	(void)fd;
+	(void)cmd;
+	errno = ENOSYS;
 	return -1;
 }
