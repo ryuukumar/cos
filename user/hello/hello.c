eab00a8dbde4c231ae24753bb1fedbbc69b0ef60
ryuukumar <adityakumar4644@gmail.com>
2026-05-01 23:10:03 +0530

    List out directory contents in hello.c

--- user/hello/hello.c
+++ user/hello/hello.c
@@ -1,12 +1,26 @@
-
+#include <dirent.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <sys/types.h>
+
+void list_dir (const char* path) {
+	printf ("\nListing: %s\n", path);
+	DIR* dir = opendir (path);
+	if (!dir) {
+		perror ("opendir");
+		return;
+	}
+
+	struct dirent* entry;
+	while ((entry = readdir (dir)) != NULL) {
+		const char* type = (entry->d_type == DT_DIR) ? "DIR" : "FILE";
+		printf ("  [%s] %s\n", type, entry->d_name);
+	}
+	closedir (dir);
+}
 
 int main (int argc, char* argv[]) {
 	printf ("Hello from USERLAND using newlib!!!\n");
-
-	for (;;)
-		;
-
-	return 1;
+	list_dir ("/");
+	list_dir ("/usr/lib");
+	return 0;
 }
