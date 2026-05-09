6cb19f7077363b414af1413ba0bfab5125d47b6a
ryuukumar <adityakumar4644@gmail.com>
2026-05-09 11:50:19 +0530

    Add varray and process queue in PCB for waitpid

--- kernel/include/kernel/process.h
+++ kernel/include/kernel/process.h
@@ -5,30 +5,34 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <utils/hashmap32.h>
+#include <utils/varray.h>
 
-typedef struct process process;
+typedef struct process		 process;
+typedef struct process_queue process_queue;
 
 typedef enum { TASK_RUNNING, TASK_READY, TASK_BLOCKED, TASK_DEAD } task_state_t;
 
 struct process {
-	uint64_t	 p_id;
-	uintptr_t	 p_cr3;
-	registers_t* p_registers_ptr;
-	task_state_t p_state;
-	process*	 next;
-	bool		 p_user;
-	uintptr_t	 p_kstack;
-	inode*		 p_wd;
-	inode*		 p_root;
-	struct file* p_fds[MAX_FDS];
-	uintptr_t	 p_heap_base;
-	uintptr_t	 p_heap_sz;
-	uintptr_t	 p_sp;
+	uint64_t	   p_id;
+	uintptr_t	   p_cr3;
+	registers_t*   p_registers_ptr;
+	task_state_t   p_state;
+	process*	   next;
+	bool		   p_user;
+	uintptr_t	   p_kstack;
+	inode*		   p_wd;
+	inode*		   p_root;
+	struct file*   p_fds[MAX_FDS];
+	uintptr_t	   p_heap_base;
+	uintptr_t	   p_heap_sz;
+	uintptr_t	   p_sp;
+	varray*		   p_children;
+	process_queue* p_waiting;
 };
 
-typedef struct {
+struct process_queue {
 	process *head, *tail;
-} process_queue;
+};
 
 process_queue* get_ready_queue (void);
 process*	   get_current_process (void);
