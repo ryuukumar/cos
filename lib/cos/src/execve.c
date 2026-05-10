9b0f6698954a360c909d5c2e9eff03a36b57e346
ryuukumar <adityakumar4644@gmail.com>
2026-05-10 15:46:17 +0530

    Remove execve definition causing build issues

--- lib/cos/src/execve.c
+++ lib/cos/src/execve.c
@@ -5,9 +5,5 @@
 // Needed for linking
 int _execve (const char* path, char* const argv[], char* const envp[]);
 int _execve (const char* path, char* const argv[], char* const envp[]) {
-	execve (path, argv, envp);
-}
-
-int execve (const char* path, char* const argv[], char* const envp[]) {
 	return syscall3 (SYSCALL_SYS_EXECVE, (uint64_t)path, (uint64_t)argv, (uint64_t)envp);
 }
