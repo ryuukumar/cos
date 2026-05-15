fb71246ac3ce72e36b5a844fe50cbdc48c10c486
ryuukumar <adityakumar4644@gmail.com>
2026-05-16 00:48:36 +0530

    Update init_con

--- kernel/include/kernel/con/con.h
+++ kernel/include/kernel/con/con.h
@@ -8,10 +8,6 @@
 #undef TAB_WIDTH
 #endif
 
-typedef struct {
-	size_t lpad, rpad, tpad, bpad;
-} pad_t;
-
 int	 add_char (unsigned char c);
-void init_con (size_t screen_width, size_t screen_height, pad_t* con_padding, size_t char_spacing,
-			   size_t line_padding, size_t font_multiplier);
+void init_con (size_t screen_width, size_t screen_height, size_t x_padding, size_t y_padding,
+			   size_t char_spacing, size_t line_padding, size_t font_multiplier);
--- kernel/src/kernel/con/con.c
+++ kernel/src/kernel/con/con.c
@@ -5,7 +5,17 @@ static console_t* console = nullptr;
 
 int add_char (unsigned char c) {}
 
-void init_con (size_t screen_width, size_t screen_height, pad_t* con_padding, size_t char_spacing,
-			   size_t line_padding, size_t font_multiplier) {
+void init_con (size_t screen_width, size_t screen_height, size_t x_padding, size_t y_padding,
+			   size_t char_spacing, size_t line_padding, size_t font_multiplier) {
+	console_parameters_t params = {.glyph_height = 8,
+								   .glyph_width = 5,
+								   .line_spacing = line_padding,
+								   .char_spacing = char_spacing,
+								   .xpad = x_padding,
+								   .ypad = y_padding,
+								   .width = screen_width,
+								   .height = screen_height,
+								   .font_size = font_multiplier};
 
+	console_create(&console, &params);
 };
