0ff1302afd9b7bf18f046de6dc4e686811c5e5b0
ryuukumar <adityakumar4644@gmail.com>
2026-05-17 14:13:50 +0530

    Update syscall numbers to local convention

--- lib/cos/include/arch/x86_64-cos/syscalls.h
+++ lib/cos/include/arch/x86_64-cos/syscalls.h
@@ -3,36 +3,46 @@
 #include <errno.h>
 #include <stdint.h>
 
-#define SYSCALL_SYS_EXIT	1
-#define SYSCALL_SYS_READ	3
-#define SYSCALL_SYS_WRITE	4
-#define SYSCALL_SYS_OPEN	5
-#define SYSCALL_SYS_CLOSE	6
-#define SYSCALL_SYS_EXECVE	11
-#define SYSCALL_SYS_LSEEK	19
-#define SYSCALL_SYS_GETPID	20
-#define SYSCALL_SYS_MKDIR	39
-#define SYSCALL_SYS_BRK		45
-#define SYSCALL_SYS_FORK	57
-#define SYSCALL_SCHED_YIELD 158
+/* File IO syscalls */
 
-#define SYSCALL_SYS_RT_SIGACTION 239
-#define SYSCALL_SYS_RT_SIGRETURN 240
-#define SYSCALL_SYS_IOCTL		 241
-#define SYSCALL_SYS_GETCWD		 242
-#define SYSCALL_SYS_CHDIR		 243
-#define SYSCALL_SYS_FCNTL		 244
-#define SYSCALL_SYS_FSTAT		 245
-#define SYSCALL_SYS_GETTIMEOFDAY 246
-#define SYSCALL_SYS_ISATTY		 247
-#define SYSCALL_SYS_KILL		 248
-#define SYSCALL_SYS_LINK		 249
-#define SYSCALL_SYS_STAT		 250
-#define SYSCALL_SYS_TIMES		 251
-#define SYSCALL_SYS_UNLINK		 252
-#define SYSCALL_SYS_WAITPID		 253
-#define SYSCALL_SYS_GETENTROPY	 254
-#define SYSCALL_SYS_GETDENTS	 255
+#define SYSCALL_SYS_READ	 1
+#define SYSCALL_SYS_WRITE	 2
+#define SYSCALL_SYS_OPEN	 3
+#define SYSCALL_SYS_CLOSE	 4
+#define SYSCALL_SYS_LSEEK	 5
+#define SYSCALL_SYS_MKDIR	 6
+#define SYSCALL_SYS_IOCTL	 7
+#define SYSCALL_SYS_GETCWD	 8
+#define SYSCALL_SYS_CHDIR	 9
+#define SYSCALL_SYS_FCNTL	 10
+#define SYSCALL_SYS_FSTAT	 11
+#define SYSCALL_SYS_ISATTY	 12
+#define SYSCALL_SYS_LINK	 13
+#define SYSCALL_SYS_STAT	 14
+#define SYSCALL_SYS_UNLINK	 15
+#define SYSCALL_SYS_GETDENTS 16
+
+/* Memory syscalls */
+
+#define SYSCALL_SYS_BRK 31
+
+/* Process management syscalls */
+
+#define SYSCALL_SYS_EXIT		 41
+#define SYSCALL_SYS_EXECVE		 42
+#define SYSCALL_SYS_GETPID		 43
+#define SYSCALL_SYS_FORK		 44
+#define SYSCALL_SCHED_YIELD		 45
+#define SYSCALL_SYS_RT_SIGACTION 46
+#define SYSCALL_SYS_RT_SIGRETURN 47
+#define SYSCALL_SYS_KILL		 48
+#define SYSCALL_SYS_TIMES		 49
+#define SYSCALL_SYS_WAITPID		 50
+
+/* Utility syscalls */
+
+#define SYSCALL_SYS_GETTIMEOFDAY 81
+#define SYSCALL_SYS_GETENTROPY	 82
 
 static inline uint64_t syscall3 (uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
 	uint64_t ret;
