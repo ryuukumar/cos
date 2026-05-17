a5078e9ef80af7ec0225433d18143183e66ff586
ryuukumar <adityakumar4644@gmail.com>
2026-05-17 15:40:29 +0530

    Modify libcos to use the syscall instruction + 0-6 arguments

--- lib/cos/include/arch/x86_64-cos/syscalls.h
+++ lib/cos/include/arch/x86_64-cos/syscalls.h
@@ -44,12 +44,69 @@
 #define SYSCALL_SYS_GETTIMEOFDAY 81
 #define SYSCALL_SYS_GETENTROPY	 82
 
+static inline uint64_t syscall0 (uint64_t num) {
+	uint64_t ret;
+	__asm__ volatile ("syscall" : "=a"(ret) : "a"(num) : "rcx", "r11", "memory");
+	return ret;
+}
+
+static inline uint64_t syscall1 (uint64_t num, uint64_t arg1) {
+	uint64_t ret;
+	__asm__ volatile ("syscall" : "=a"(ret) : "a"(num), "D"(arg1) : "rcx", "r11", "memory");
+	return ret;
+}
+
+static inline uint64_t syscall2 (uint64_t num, uint64_t arg1, uint64_t arg2) {
+	uint64_t ret;
+	__asm__ volatile ("syscall"
+					  : "=a"(ret)
+					  : "a"(num), "D"(arg1), "S"(arg2)
+					  : "rcx", "r11", "memory");
+	return ret;
+}
+
 static inline uint64_t syscall3 (uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
 	uint64_t ret;
-	__asm__ volatile ("int $0x80"
+	__asm__ volatile ("syscall"
 					  : "=a"(ret)
-					  : "a"(num), "D"(arg1), "S"(arg2), "d"(arg3) // rdi, rsi, rdx
-					  : "memory");
+					  : "a"(num), "D"(arg1), "S"(arg2), "d"(arg3)
+					  : "rcx", "r11", "memory");
+	return ret;
+}
+
+static inline uint64_t syscall4 (uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3,
+								 uint64_t arg4) {
+	uint64_t		  ret;
+	register uint64_t r10 __asm__ ("r10") = arg4;
+	__asm__ volatile ("syscall"
+					  : "=a"(ret)
+					  : "a"(num), "D"(arg1), "S"(arg2), "d"(arg3), "r"(r10)
+					  : "rcx", "r11", "memory");
+	return ret;
+}
+
+static inline uint64_t syscall5 (uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3,
+								 uint64_t arg4, uint64_t arg5) {
+	uint64_t		  ret;
+	register uint64_t r10 __asm__ ("r10") = arg4;
+	register uint64_t r8 __asm__ ("r8") = arg5;
+	__asm__ volatile ("syscall"
+					  : "=a"(ret)
+					  : "a"(num), "D"(arg1), "S"(arg2), "d"(arg3), "r"(r10), "r"(r8)
+					  : "rcx", "r11", "memory");
+	return ret;
+}
+
+static inline uint64_t syscall6 (uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3,
+								 uint64_t arg4, uint64_t arg5, uint64_t arg6) {
+	uint64_t		  ret;
+	register uint64_t r10 __asm__ ("r10") = arg4;
+	register uint64_t r8 __asm__ ("r8") = arg5;
+	register uint64_t r9 __asm__ ("r9") = arg6;
+	__asm__ volatile ("syscall"
+					  : "=a"(ret)
+					  : "a"(num), "D"(arg1), "S"(arg2), "d"(arg3), "r"(r10), "r"(r8), "r"(r9)
+					  : "rcx", "r11", "memory");
 	return ret;
 }
 
