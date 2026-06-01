a285e45fb2dcd62d323e6770526444a7a4135af3
ryuukumar <adityakumar4644@gmail.com>
2026-06-01 22:46:08 +0530

    Stub some functions

--- lib/cos/src/stubs.c
+++ lib/cos/src/stubs.c
@@ -17,7 +17,9 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #undef errno
 extern int errno;
@@ -29,5 +31,45 @@ int fcntl (int fd, int cmd, ...) {
 	(void)fd;
 	(void)cmd;
 	errno = ENOSYS;
+	return -1;
+}
+
+int gettimeofday (struct timeval* __restrict __p, void* __restrict __tz) {
+	(void)__p, (void)__tz;
+	errno = ENOSYS;
+	return -1;
+}
+
+int access (const char* __path, int __amode) {
+	(void)__path, (void)__amode;
+	errno = ENOENT;
+	return -1;
+}
+
+mode_t umask (mode_t __mask) {
+	(void)__mask;
+	return 0;
+}
+
+int chmod (const char* __path, mode_t __mode) {
+	(void)__path, (void)__mode;
+	return 0;
+}
+
+long sysconf (int __name) {
+	if (__name == _SC_OPEN_MAX) return 64;
+	errno = EINVAL;
+	return -1;
+}
+
+int dup (int __fildes) {
+	(void)__fildes;
+	errno = -ENOSYS;
+	return -1;
+}
+
+int dup2 (int __fildes, int __fildes2) {
+	(void)__fildes, (void)__fildes2;
+	errno = -ENOSYS;
 	return -1;
 }
