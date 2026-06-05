d4d9ea6ff2028318443a39b3f250bf92041ba9b3
ryuukumar <adityakumar4644@gmail.com>
2026-06-05 19:03:36 +0530

    Format

--- kernel/src/kernel/fs/vfs/rmdir.c
+++ kernel/src/kernel/fs/vfs/rmdir.c
@@ -36,7 +36,7 @@ int do_rmdir (const char* path) {
 	error = lookup_inode_by_path (name, current->p_root, parent, &node,
 								  L_DCHK | (error == 1 ? L_FLNK : 0));
 	if (error) goto cleanup;
-    
+
 	if (!node->i_iops || !node->i_iops->rmdir) {
 		error = -ENOSYS;
 		goto cleanup;
